b4045b8d9972b61393a5b3a28189a2d501ec50a2
Carson Gulledge <carsongulledge@gmail.com>
2016-02-07 22:45:25 -0800

    CreateEventActivity comments

--- app/src/main/java/cse403/blast/CreateEventActivity.java
+++ app/src/main/java/cse403/blast/CreateEventActivity.java
@@ -24,6 +24,12 @@ import java.util.Date;
 import cse403.blast.Support.DatePickerFragment;
 import cse403.blast.Support.TimePickerFragment;
 
+/**
+ * CreateEventActivity allows a user to create a new Event. After inputting valid data for each
+ * of the Event fields, the user clicks "Blast It!" to publish the Event and make it visible to
+ * other users within the appropriate location radius. The Event is then displayed under the device
+ * user's "Blasts You Created" section of the main page's drawer.
+ */
 public class CreateEventActivity extends AppCompatActivity {
 
     private final String TAG = "CreateEventActivity";
@@ -37,7 +43,7 @@ public class CreateEventActivity extends AppCompatActivity {
 
         Intent createEventIntent = getIntent();
 
-
+        // sets up the listener for displaying the date picker
         EditText dateText = (EditText) findViewById(R.id.date);
         dateText.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -48,6 +54,7 @@ public class CreateEventActivity extends AppCompatActivity {
             }
         });
 
+        // sets up the listener for displaying the time picker
         EditText timeText = (EditText) findViewById(R.id.time);
         timeText.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -63,18 +70,32 @@ public class CreateEventActivity extends AppCompatActivity {
 
     // DIALOG METHODS
 
+    /**
+     * Displays the date picker
+     *
+     * @param v The View to display to
+     */
+    // TODO: Limit the dates that a user can choose from on the calendar
     private void showDatePickerDialog(View v) {
         DatePickerFragment newFragment = DatePickerFragment.newInstance(
                 new Date(Calendar.getInstance().getTimeInMillis()), dateSetListener);
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
+    /**
+     * Displays the time picker
+     *
+     * @param v The View to display to
+     */
     private void showTimePickerDialog(View v) {
         TimePickerFragment newFragment = TimePickerFragment.newInstance(
                 new Date(Calendar.getInstance().getTimeInMillis()), getTimeSetListener());
         newFragment.show(getSupportFragmentManager(), "timePicker");
     }
 
+    /**
+     * Date picker on set listener
+     */
     // TODO: Use Calendar set to create a reliable, format independent date
     private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
         @Override
@@ -82,22 +103,34 @@ public class CreateEventActivity extends AppCompatActivity {
             EditText tackDate = (EditText) findViewById(R.id.date);
             tackDate.setText((monthOfYear + 1) + "/" + dayOfMonth + "/" + year);
             Log.i("Tag", "set");
+            // make the time picker display after choosing a date
             getTimePickerDialog().show(getSupportFragmentManager(), "timePicker");
         }
     };
 
+    /**
+     * Gets the time picker
+     *
+     * @return DialogFragment
+     */
     private DialogFragment getTimePickerDialog() {
         DialogFragment newFragment = TimePickerFragment.newInstance(
                 new Date(Calendar.getInstance().getTimeInMillis()), getTimeSetListener());
         return newFragment;
     }
 
+    /**
+     * Time picker on set listener
+     *
+     * @return TimePickerDialog.OnTimeSetListener
+     */
+    // TODO: Format time to not be in military
     private TimePickerDialog.OnTimeSetListener getTimeSetListener() {
         return new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                 EditText tackTime = (EditText) findViewById(R.id.time);
-                tackTime.setText("" + hourOfDay + " " + minute);
+                tackTime.setText("" + hourOfDay + ":" + minute);
             }
         };
     }
