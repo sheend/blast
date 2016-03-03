570fc719ebe4a3a88ec994d0a064ee597653f7ef
graceqiu <grace.qiu.r@gmail.com>
2016-03-02 23:57:14 -0800

    DetailActivity commented, has its drawer tests but they don't pass b/c drawer won't goddamn open I give up WHERE IS THE GODDAMN HAMBURGER

--- app/src/androidTest/java/cse403/blast/DetailActivityTest.java
+++ app/src/androidTest/java/cse403/blast/DetailActivityTest.java
@@ -3,22 +3,36 @@ package cse403.blast;
 import cse403.blast.Model.Event;
 import cse403.blast.Model.User;
 import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.View;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import java.util.Date;
+
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+// FOR GODDAMN DRAWER: import static android.support.test.espresso.contrib.DrawerActions.openDrawer;
 
 /**
  * Created by graceqiu on 2/17/16.
  *
- * Tests DetailActivity class.
+ * Tests DetailActivity class. User should be able to join events not currently attending,
+ * should be able to leave events they are currently attending, and should be able to
+ * edit an event if they are the owner. The button displayed on the activity should change
+ * depending on the user's relationship to the event. Furthermore, the left drawer should update
+ * when a user attends or leaves an event.
  */
 
 @RunWith(AndroidJUnit4.class)
@@ -61,10 +75,34 @@ public class DetailActivityTest {
     }
 
     @Test
-    public void strangerButtonDisplay() {
+    public void shouldDisplayJoinIfStranger() {
         Event e = new Event("strangerTest", "strangerTitle", "strangerDesc", "strangerLoc", 100, new Date(1));
         launchActivity(e);
         onView(withId(R.id.detail_button)).check(matches(withText(R.string.detail_join)));
+    }
+
+    // TODO: drawer won't open
+    @Test
+    public void shouldPopulateDrawerAttendingListIfAttending() {
+        Event e = new Event("strangerTest", "strangerTitle", "strangerDesc", "strangerLoc", 100, new Date(1));
+        launchActivity(e);
+        onView(withId(R.id.detail_button)).perform(ViewActions.click());
+        //onView(withContentDescription("Open navigation")).perform(ViewActions.click());
+        DrawerLayout drawer = (DrawerLayout) rule.getActivity().findViewById(R.id.drawer_layout);
+        drawer.openDrawer(Gravity.LEFT);
+        onData(anything()).inAdapterView(withId(R.id.attending_list)).atPosition(0).check(matches(withText("strangerTitle")));
+    }
+
+    // TODO: drawer won't open
+    @Test
+    public void shouldUpdateDrawerAttendingListIfLeaving() {
+        Event e = new Event("strangerTest", "strangerTitle", "strangerDesc", "strangerLoc", 100, new Date(1));
+        e.addAttendee(testUser);
+        launchActivity(e);
+        onView(withId(R.id.detail_button)).perform(ViewActions.click());
+        //onView(withContentDescription("Open navigation")).perform(ViewActions.click());
+        onView(withId(R.id.nav_view)).perform(ViewActions.click());
+        onData(anything()).inAdapterView(withId(R.id.attending_list)).atPosition(0).check(matches(withText("strangerTitle")));
     }
 
     /**
